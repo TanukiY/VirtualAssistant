7467fb181389468d7e651608f5d5ea3ecd896091
nds27 <nds27@LAPTOP-S7OLFR4E>
2023-04-08 14:28:10 +0300

    add: classifier class and LevenstrinDistance. Test command открой

---
+++ VirtualAssistant/Classifier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualAssistant
+{
+    internal class Classifier
+    {
+        private IEnumerable<DictionaryCmd> data;
+        public void addDictionaryCmd(IEnumerable<DictionaryCmd> dictionaryCmd)
+        {
+            this.data = dictionaryCmd;
+        }
+        public string Predict(string synonym)
+        {
+            DictionaryCmd currentBest = null;
+            var shortest = Double.MaxValue;
+            foreach (DictionaryCmd obs in this.data)
+            {
+                double dist = LevenshteinDistance.Between(obs.Synonym, synonym);
+                if (dist < shortest)
+                {
+                    shortest = dist;
+                    currentBest = obs;
+                }
+            }
+            return currentBest.Reference;
+        }
+    }
+}
