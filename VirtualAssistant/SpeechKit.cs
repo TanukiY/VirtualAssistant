e48b22946e00cc0aa9a52fd6cd8635bd8091457f
TanukiY <92318683+TanukiY@users.noreply.github.com>
2023-05-24 23:22:47 +0300

    add: class SpeechKit

---
+++ VirtualAssistant/SpeechKit.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualAssistant.Properties;
+
+namespace VirtualAssistant
+{    
+    internal class SpeechKit
+    {
+        private static string ApiURL { get; set; }
+        private SpeechKit()
+        {
+            ApiURL = "https://stt.api.cloud.yandex.net/speech/v1/stt:recognize";
+        }
+        async static private Task<string> ConvertSpeechToText(byte[] data)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, ApiURL);
+            client.DefaultRequestHeaders.Authorization 
+                = new AuthenticationHeaderValue(
+                    "Bearer", YCloudSetting.GetInstance().YandexCloudToken);
+
+            string url = $"{ApiURL}" +
+                         $"?folderId={YCloudSetting.GetInstance().YandexCloudFolderId}" +
+                         $"&topic=general" +
+                         $"&lang=ru-RU" +
+                         $"&profanityFilter=false" +
+                         $"&rawResults=false" +
+                         $"&sampleRateHertz=48000" +
+                         $"&format=lpcm";
+
+            var response = await client.PostAsync(url, new ByteArrayContent(data));
+
+            string responseStream = "";
+            if (response.IsSuccessStatusCode)
+            {
+                responseStream = await response.Content.ReadAsStringAsync();
+            }
+
+            string result = "";
+            try
+            {
+                var jobj = JObject.Parse(responseStream);
+                result = jobj["result"].ToString();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Не удалось сформировать JSON файл");
+            }
+
+            return result;
+        }
+    }
+}
